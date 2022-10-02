7e0f4dd914b1555c6d0a9f63efdb9245b0687381
rafia <safinarafia@gmail.com>
2022-10-02 18:52:36 +0300

    ion

---
+++ loginApp/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  loginApp
+//
+//  Created by Рафия Сафина on 02.10.2022.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+    @IBAction func logOutButtonTapped() {
+    }
+}
