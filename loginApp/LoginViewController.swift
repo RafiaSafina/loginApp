5435ac7e2e4ebe375ad5a9d7fe7b02b46c989302
rafia <safinarafia@gmail.com>
2022-10-02 17:10:23 +0300

    setup alert buttons

--- loginApp/LoginViewController.swift
+++ loginApp/LoginViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    let userName = "User"
+    let password = "password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func logInButton() {
+        
+    }
+    
+    @IBAction func showUserNameButtonTapped() {
+        let alert = UIAlertController(title: "Ooops!", message: "Your user name is \(userName)", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func showPasswordButtonTapped() {
+        let alert = UIAlertController(title: "Ooops!", message: "Your password is \(password)", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
