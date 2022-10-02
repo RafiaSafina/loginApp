6861965bb6f75a7f18f7eb547fc3d04fa4e23a65
rafia <safinarafia@gmail.com>
2022-10-02 22:19:41 +0300

    work with logic

--- loginApp/LoginViewController.swift
+++ loginApp/LoginViewController.swift
@@ -17,44 +17,47 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.username = addNameTextField.text
     }
     
     @IBAction func logInButton() {
-        switch (addNameTextField.text,passwordTextField.text) {
-        case ("User", "password"):
-            print("ok")
-        default:
+        if (addNameTextField.text,  passwordTextField.text) != (userName, password) {
             showWrongDataAlert()
             passwordTextField.text = ""
         }
     }
     
     @IBAction func showUserNameButtonTapped() {
-        let alert = UIAlertController(title: "Ooops!", message: "Your user name is \(userName)", preferredStyle: UIAlertController.Style.alert)
-        
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        
-        self.present(alert, animated: true, completion: nil)
+        showAlert(title: "Ooops!", message: "Your user name is \(userName)!", bottomTitle: "OK")
     }
     
     
     @IBAction func showPasswordButtonTapped() {
-        let alert = UIAlertController(title: "Ooops!", message: "Your password is \(password)", preferredStyle: UIAlertController.Style.alert)
-        
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        
-        self.present(alert, animated: true, completion: nil)
+        showAlert(title: "Ooops!", message: "Your password is \(password)!", bottomTitle: "OK")
     }
     
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        addNameTextField.text = ""
+        passwordTextField.text = ""
+    }
     
     private func showWrongDataAlert() {
-        let alert = UIAlertController(title: "Invalid login or password", message: "Please, enter correct login and password", preferredStyle: UIAlertController.Style.alert)
-        
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        
+        showAlert(title: "Invalid login or password", message: "Please, enter correct login and password", bottomTitle: "OK")
+    }
+     
+    private func showAlert(title: String, message: String, bottomTitle: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: bottomTitle, style: UIAlertAction.Style.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
 }
 
--- loginApp/WelcomeViewController.swift
+++ loginApp/WelcomeViewController.swift
@@ -9,15 +9,13 @@ import UIKit
 
 final class WelcomeViewController: UIViewController {
 
+    @IBOutlet var userName: UILabel!
+    
+    var username: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        userName.text = username
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-
-    @IBAction func logOutButtonTapped() {
-    }
 }
